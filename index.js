f82df304de221fec7064700cc738d05137dda701
David Stewart <davidstewart@gmail.com>
2018-12-31 03:23:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 var list = []
 function theBeatlesPlay(music, instruments) {
-  for (v = 0; v < music.length; v++) {
+  for (let v = 0; v < music.length; v++) {
     list.push(`${music[v]} plays ${instruments[v]}`)
   }
   return list 
