27378a179e9631e346958b0f1f29c97ed3628b6c
David Stewart <davidstewart@gmail.com>
2018-12-31 03:20:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var list = []
 function theBeatlesPlay(music, instruments) {
   for (v = 0; v < music.length; v++) {
   console.log(`${music[v]} plays ${instruments[v]}`)
-    list.push()
+    list.push(`${music[v]} plays ${instruments[v]}`)
   }
+  return list 
 }
   
 
